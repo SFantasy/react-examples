6fd8fce0ebdadbbd6788ea4651ff4226cb7a416d
Fantasy Shao <forever.fantasy27@gmail.com>
2016-03-08 00:11:55 +0800

    Add SimpleComponent

---
+++ SimpleComponent/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var SimpleRenderer = require('./SimpleRenderer');
+
+ReactDOM.render(
+  <SimpleRenderer />,
+  document.getElementById('container')
+);
